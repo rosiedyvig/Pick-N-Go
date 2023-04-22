45f295c0d7e01518b62aa322c9a0fe47cd0b29db
Rosie Dyvig <rosiedyvig@gmail.com>
2023-04-22 12:56:04 +0200

    feat: Home page styles and font added

--- Client/screens/Home.js
+++ Client/screens/Home.js
@@ -1,19 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { View, Text, StyleSheet, ImageBackground, Image } from "react-native";
 import Button from "../components/Button";
 import Login from "../components/SignUp";
 import SignUp from "../components/Login";
 
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
 const logo = require("../assets/logo.png");
 const grass = require("../assets/grass.avif");
 
 const Home = ({ navigation }) => {
+  let [fontsLoaded] = useFonts({
+    BarlowSemiBold: require("../assets/Barlow,Inter,Overpass/Barlow/Barlow-SemiBold.ttf"),
+    BarlowRegular: require("../assets/Barlow,Inter,Overpass/Barlow/Barlow-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <ImageBackground source={grass} resizeMode="cover" style={styles.image}>
+    <ImageBackground
+      source={grass}
+      resizeMode="cover"
+      style={styles.image}
+      onLayout={onLayoutRootView}
+    >
       <View style={styles.container}>
         <Image style={styles.logo} source={logo}></Image>
         <View style={styles.header}>
-          <Text style={styles.text}>Are You A Club or a Player?</Text>
+          <Text style={[styles.text, { fontWeight: "bold" }]}>
+            Keep the game alive with Pick N Go!{" "}
+          </Text>
+          <Text style={styles.text}>
+            Connect with other clubs and help them fill their squad. Post a
+            position or fixture that you need filled!
+          </Text>
+          <Text style={styles.text}>
+            Say goodbye to cancelled fixtures and hello to more Rugby Sundays!
+          </Text>
         </View>
 
         <View style={styles.buttoncontainer}>
@@ -38,24 +72,30 @@ const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: "teal",
   },
   buttoncontainer: {
     flexDirection: "row",
     justifyContent: "space-around",
   },
   header: {
-    marginTop: 150,
-    backgroundColor: "red", //TO BE DELETED
-    alignItems: "center",
+    marginVertical: 20,
+    marginHorizontal: 20,
+    borderRadius: 15,
+    borderColor: "#9CCC67",
+    borderWidth: 9,
+    backgroundColor: "rgba(206, 245, 206, 0.91)",
     paddingVertical: 10,
+    paddingHorizontal: 10,
   },
   text: {
-    fontWeight: "bold",
-    fontSize: 26,
-    color: "white",
-    alignContent: "center",
-    justifyContent: "center",
+    fontSize: 18,
+    color: "black",
+    paddingVertical: 10,
+
+    // alignItems: "center",
+    // justifyContent: "center",
+    // alignSelf: "center",
+    textAlign: "center",
   },
   image: {
     flex: 1,
